226dc91c7f1dc735d9409c7d471e55c3b0ab00d3
CPPrestiged <84301927+CPPrestiged@users.noreply.github.com>
2022-06-16 14:14:09 -0700

    Extra features, bug fixes, files, etc.

--- EsoTargs/Program.cs
+++ EsoTargs/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace EsoTargs
@@ -9,12 +10,16 @@ namespace EsoTargs
         static int current = 0;
         static int pointerLoc = 0;
         static List<int> vals = new List<int>();
-        static string code = "n9 w9";
-        static string[] commands = code.Split(" ");
+        static string code = "hola";
+        static string[] commands;
+
+        static string filename = "script";
 
         static bool lineStarted = false;
         static void Main(string[] args)
         {
+            code = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @filename + ".tgs");
+            commands = code.Trim().Split(" ");
             for (int i = 0; i < 69420; i++)
             {
                 vals.Add(0);
@@ -23,7 +28,7 @@ namespace EsoTargs
         }
         static void runArg()
         {
-            string cmd = (commands[current])[0].ToString();
+            string cmd = (commands[current])[0].ToString().Trim();
             string val = (commands[current])[1].ToString();
 
             switch (cmd)
@@ -45,15 +50,36 @@ namespace EsoTargs
                     {
                         Console.Write("\n");
                     }
+                    if (val.ToLower().Equals("p"))
+                    {
+                        Console.Write(Encoding.ASCII.GetString(new byte[] { (byte)(vals[pointerLoc]) }));
+                        lineStarted = true;
+                        break;
+                    }
                     Console.Write(Encoding.ASCII.GetString(new byte[] { (byte)(vals[int.Parse(val)]) }));
                     lineStarted = true;
                     break;
                 case "w":
+                    if (val.ToLower().Equals("p"))
+                    {
+                        Console.Write(Encoding.ASCII.GetString(new byte[] { (byte)(vals[pointerLoc]) }));
+                        lineStarted = true;
+                        break;
+                    }
                     Console.Write(Encoding.ASCII.GetString(new byte[] { (byte)(vals[int.Parse(val)]) }));
                     lineStarted = true;
                     break;
                 case "i":
                     current += 1;
+
+                    if (val.ToLower().Equals("p"))
+                    {
+                        if (vals[pointerLoc] != 0)
+                        {
+                            runArg();
+                        }
+                        break;
+                    }
 
                     if (vals[int.Parse(val)] != 0)
                     {
@@ -69,10 +95,21 @@ namespace EsoTargs
                     {
                         i = 0;
                     }
+                    if (val.ToLower().Equals("p"))
+                    {
+                        vals[pointerLoc] = i;
+                        break;
+                    }
                     vals[int.Parse(val)] = i;
                     break;
                 case "c":
                     char sc = Console.ReadKey().KeyChar;
+
+                    if (val.ToLower().Equals("p"))
+                    {
+                        vals[pointerLoc] = (int)sc;
+                        break;
+                    }
 
                     vals[int.Parse(val)] = (int)sc;
 
